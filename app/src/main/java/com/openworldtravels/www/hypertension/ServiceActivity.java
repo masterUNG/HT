6bf1229fcb53c0d4d38375a7941fb59933677982
masterUNG <phrombutr@gmail.com>
2016-10-20 17:13:20 +0700

    finish

--- app/src/main/java/com/openworldtravels/www/hypertension/ServiceActivity.java
+++ app/src/main/java/com/openworldtravels/www/hypertension/ServiceActivity.java
@@ -2,6 +2,8 @@ package com.openworldtravels.www.hypertension;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class ServiceActivity extends AppCompatActivity {
@@ -18,13 +20,24 @@ public class ServiceActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.livUser);
 
         //Receive Value From Intent
-        String[] nameStrings = getIntent().getStringArrayExtra("Name");
+        final String[] nameStrings = getIntent().getStringArrayExtra("Name");
         String[] iconStrings = getIntent().getStringArrayExtra("Image");
 
         //Create ListView
         UserAdapter userAdapter = new UserAdapter(ServiceActivity.this, nameStrings,
                 iconStrings);
         listView.setAdapter(userAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                MyAlert myAlert = new MyAlert(ServiceActivity.this,
+                        nameStrings[i], "รายละเอียด");
+                myAlert.myDialog();
+
+            }   // onItemClick
+        });
 
     }   // Main Method
 
