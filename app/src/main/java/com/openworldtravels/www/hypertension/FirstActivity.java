347ed8372ef39eb16c01efb14a30d0596487abbd
masterUNG <phrombutr@gmail.com>
2016-10-20 15:44:53 +0700

    Read JSON finish

--- app/src/main/java/com/openworldtravels/www/hypertension/FirstActivity.java
+++ app/src/main/java/com/openworldtravels/www/hypertension/FirstActivity.java
@@ -1,15 +1,26 @@
 package com.openworldtravels.www.hypertension;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class FirstActivity extends AppCompatActivity {
 
     //Explicit
     private Button signInButton, signUpButton;
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,37 @@ public class FirstActivity extends AppCompatActivity {
         //Bind Widget
         signInButton = (Button) findViewById(R.id.button);
         signUpButton = (Button) findViewById(R.id.button2);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+
+        //signIn Controller
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Get Value
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (userString.equals("") || passwordString.equals("")) {
+                    //Have Space
+                    MyAlert myAlert = new MyAlert(FirstActivity.this, "มีช่องว่าง",
+                            "กรุณากรอกทุกช่อง คะ");
+                    myAlert.myDialog();
+
+                } else {
+                    //No Space
+                    MyConstant myConstant = new MyConstant();
+                    SynUser synUser = new SynUser(FirstActivity.this);
+                    synUser.execute(myConstant.getUrlAllJSONString());
+
+                }
+
+            }   // onClick
+        });
+
+
 
         //signUp Controller
         signUpButton.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +75,42 @@ public class FirstActivity extends AppCompatActivity {
 
     }   // Main Method
 
+    private class SynUser extends AsyncTask<String, Void, String> {
 
+        //Explicit
+        private Context context;
+
+        public SynUser(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(strings[0]).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("20octV2", "JSON ==> " + s);
+
+
+        }   // onPost
+
+    }   // SynUser Class
 
 }   // Main Class
